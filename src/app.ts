b22223487cb284402003cdb12d1adac5917c3da2
Andres Meza <afmeza51@misena.edu.co>
2024-10-29 23:21:53 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ function setupMiddlewares(app: Express) {
   app.use(
     cors({
       origin: "https://chibataclientv1-production.up.railway.app", // Dominio del cliente
-      methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Métodos HTTP permitidos
       credentials: true,
     })
   );
